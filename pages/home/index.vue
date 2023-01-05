d405eb472b59ad0881696dcb7fb02787e8efeb9a
CZH20010104 <2247632489@qq.com>
2023-01-05 18:03:53 +0800

    搭建项目底部tab栏

---
+++ pages/home/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="bg">
+		
+	</view>
+</template>
+
+<script setup>
+	
+</script>
+
+<style lang="scss" scoped>
+.bg {
+	width: 100%;
+	position: fixed;
+	bottom: 0;
+	right: 0;
+	top: 0;
+	left: 0;
+	background-color: #FBFBFB;
+	overflow-y: hidden;
+}
+</style>
---
+++ pages/user/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<h1>我的</h1>
+</template>
+
+<script setup>
+	
+</script>
+
+<style>
+
+</style>

f72e8f9dcfba13ce64435a672507202c9dfe39ac
CZH20010104 <2247632489@qq.com>
2023-01-06 17:41:26 +0800

    完成首页静态

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,22 +1,365 @@
 <template>
 	<view class="bg">
-		
+		<view class="search">
+			<view class="logoBg"></view>
+			<view class="searchBox">
+				<u-search shape="round" v-model="keyword" :show-action="false" @search="changeCancel"></u-search>
+			</view>
+		</view>
+		<view class="context">
+			<view class="wrap">
+				<u-swiper :list="swiperList" name="image" border-radius="20" height="278" @click="changeSwiper">
+				</u-swiper>
+			</view>
+			<view class="news">
+				<view class="newstitle">新闻</view>
+				<view class="newstext">农场发展新模式-向往生活</view>
+				<view class="newsdite">通知</view>
+			</view>
+			<view class="fundomian">
+				<view class="fundomianItem">
+					<view class="itemBg1"></view>
+					<view class="itemText">积分</view>
+				</view>
+				<view class="fundomianItem">
+					<view class="itemBg2"></view>
+					<view class="itemText">农场</view>
+				</view>
+				<view class="fundomianItem">
+					<view class="itemBg3"></view>
+					<view class="itemText">商城入驻</view>
+				</view>
+				<view class="fundomianItem">
+					<view class="itemBg4"></view>
+					<view class="itemText">交易所</view>
+				</view>
+			</view>
+			<view class="indexTitle">
+				<img src="@/static/img/indexTitleBg.png" alt="">
+			</view>
+			<view class="tabBox">
+				<u-tabs 
+					height="70" 
+					bg-color="#FBFBFB" duration="0.3" gutter="15" active-color="#3A2A08" bar-width="60"
+					bar-height="8" :bar-style="{
+						'background-color': '#1FA848',
+						'color': '#3A2A08',
+						'text-align': 'center',
+						'font-size': '24rpx',
+						'margin': '0rpx 0rpx',
+					}" :active-item-style="{
+						'font-size': '32rpx',
+						'color': '#1FA848',
+					}" :list="tabList" v-model="current" @change="changeTabs"></u-tabs>
+			</view>
+			<view class="ctxList">
+				<view class="ctxListItem">
+					<view class="ItemTop">
+						<img src="@/static/img/logoBg.png" alt="">
+						<text class="ItemTitle">数码电子</text>
+					</view>
+					<view class="ItemBottom">
+						<view class="ItemBottomCtx">
+							<img src="@/static/img/camera.png" alt="">
+							<view class="ItemBottomText">
+								<text>数码相机</text>
+								<view class="goldCtx">
+									<text class="gold">3000</text>
+									<text class="goldForm">/元</text>
+								</view>
+							</view>
+						</view>
+						<view class="ItemBottomBtn">购买</view>
+					</view>
+				</view>
+				<view class="ctxListItem">
+					<view class="ItemTop">
+						<img src="@/static/img/logoBg.png" alt="">
+						<text class="ItemTitle">数码电子</text>
+					</view>
+					<view class="ItemBottom">
+						<view class="ItemBottomCtx">
+							<img src="@/static/img/watches.png" alt="">
+							<view class="ItemBottomText">
+								<text>时尚手表</text>
+								<view class="goldCtx">
+									<text class="gold">3000</text>
+									<text class="goldForm">/元</text>
+								</view>
+							</view>
+						</view>
+						<view class="ItemBottomBtn">购买</view>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script setup>
-	
+	import {
+		reactive,
+		toRefs
+	} from "vue";
+	const data = reactive({
+		keyword: '',
+		tabList: [
+			{name: '全部'},
+			{name: '康养'},
+			{name: '电子'},
+		],
+		current: 0,
+		swiperList: [{
+				image: 'https://cdn.uviewui.com/uview/swiper/1.jpg',
+				title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+			},
+			{
+				image: 'https://cdn.uviewui.com/uview/swiper/2.jpg',
+				title: '身无彩凤双飞翼，心有灵犀一点通'
+			},
+			{
+				image: 'https://cdn.uviewui.com/uview/swiper/3.jpg',
+				title: '谁念西风独自凉，萧萧黄叶闭疏窗，沉思往事立残阳'
+			}
+		],
+	})
+	const {
+		keyword,
+		swiperList,
+		tabList,
+		current
+	} = toRefs(data)
+	// 用户点击搜索后触发
+	const changeCancel = () => {
+		console.log(keyword.value)
+	}
+	// 用户点击轮播图后触发
+	const changeSwiper = (index) => {
+		console.log(index)
+	}
+	// 用户点击tabs后触发
+	const changeTabs = (index) => {
+		console.log(index)
+	}
 </script>
 
 <style lang="scss" scoped>
-.bg {
-	width: 100%;
-	position: fixed;
-	bottom: 0;
-	right: 0;
-	top: 0;
-	left: 0;
-	background-color: #FBFBFB;
-	overflow-y: hidden;
-}
+	.bg {
+		width: 100%;
+		background-color: #FBFBFB;
+	}
+	
+	.ctxList {
+		width: 100%;
+		margin-top: 32rpx;
+	}
+	
+	.ctxListItem {
+		width: 686rpx;
+		height: 248rpx;
+		background: #FFFFFF;
+		box-shadow: 0rpx 6rpx 12rpx 2rpx rgba(0,0,0,0.16);
+		border-radius: 16rpx 16rpx 16rpx 16rpx;
+		padding: 0 34rpx;
+		font-size: 28rpx;
+		color: #3A2A08;
+		font-weight: bold;
+		margin-bottom: 32rpx;
+		.ItemBottom {
+			height: 166rpx;
+			width: 100%;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			img {
+				width: 120rpx;
+				height: 120rpx;
+			}
+			.ItemBottomCtx {
+				display: flex;
+				align-items: center;
+			}
+			
+			.ItemBottomText {
+				margin-left: 18rpx;
+			}
+			.ItemBottomBtn {
+				width: 126rpx;
+				height: 48rpx;
+				background: #24743C;
+				border-radius: 24rpx 24rpx 24rpx 24rpx;
+				color: #FFF;
+				font-size: 28rpx;
+				font-weight: bold;
+				text-align: center;
+				line-height: 48rpx;
+			}
+			.goldCtx {
+				margin-top: 16rpx;
+			}
+			.gold {
+				color: #E60808;
+			}
+			.goldForm {
+				font-size: 20rpx;
+				font-weight: bold;
+				color: #3A0808;
+			}
+		}
+		.ItemTop {
+			width: 100%;
+			height: 82rpx;
+			border-bottom: 2rpx solid rgba(112, 112, 112, 0.08);
+			display: flex;
+			align-items: center;
+			.ItemTitle {
+				margin-left: 22rpx;
+			}
+			img {
+				width: 42rpx;
+				height: 42rpx;
+			}
+		}
+	}
+
+	.search {
+		width: 750rpx;
+		height: 394rpx;
+		background: #24743C;
+		box-shadow: 0rpx 6rpx 12rpx 2rpx rgba(0, 0, 0, 0.16);
+		border-radius: 0rpx 0rpx 60rpx 60rpx;
+		padding: 0 32rpx;
+		display: flex;
+		justify-content: space-between;
+
+		.logoBg {
+			width: 52rpx;
+			height: 52rpx;
+			background: url("@/static/img/logo.png") 100% no-repeat;
+			background-size: 100% 100%;
+			margin-top: 135rpx;
+		}
+
+		.searchBox {
+			width: 604rpx;
+			height: 64rpx;
+			margin-top: 132rpx;
+		}
+	}
+
+	.context {
+		width: 100%;
+		padding: 0 32rpx;
+		margin-top: -172rpx;
+	}
+
+	.indexTitle {
+		margin-top: 32rpx;
+		width: 686rpx;
+		height: 130rpx;
+		margin-bottom: 10rpx;
+		img {
+			width: 100%;
+			height: 100%;
+			box-shadow: 0rpx 6rpx 12rpx 2rpx rgba(0,0,0,0.16);
+			border-radius: 16rpx 16rpx 16rpx 16rpx;
+		}
+	}
+
+	.news {
+		width: 686rpx;
+		height: 88rpx;
+		background: #FFFFFF;
+		box-shadow: 0rpx 6rpx 12rpx 2rpx rgba(0, 0, 0, 0.16);
+		border-radius: 20rpx 20rpx 20rpx 20rpx;
+		margin: 16rpx 0rpx;
+		display: flex;
+		align-items: center;
+		padding: 0 20rpx;
+		justify-content: space-between;
+
+		.newstitle {
+			width: 80rpx;
+			height: 42rpx;
+			background: #2F9F50;
+			border-radius: 22rpx 22rpx 22rpx 22rpx;
+			font-size: 24rpx;
+			font-weight: 400;
+			color: #FFFFFF;
+			text-align: center;
+			line-height: 42rpx;
+		}
+
+		.newstext {
+			font-size: 24rpx;
+			font-weight: 400;
+			color: #6E6E6E;
+		}
+
+		.newsdite {
+			width: 68rpx;
+			height: 38rpx;
+			border-radius: 20rpx 20rpx 20rpx 20rpx;
+			border: 2rpx solid #FF0000;
+			font-size: 24rpx;
+			font-weight: bold;
+			color: #FF0000;
+			text-align: center;
+			line-height: 38rpx;
+		}
+	}
+
+	.fundomian {
+		width: 686rpx;
+		height: 178rpx;
+		background: #FFFFFF;
+		box-shadow: 0rpx 6rpx 12rpx 2rpx rgba(0, 0, 0, 0.16);
+		border-radius: 16rpx 16rpx 16rpx 16rpx;
+		font-size: 24rpx;
+		font-weight: 400;
+		color: #3A2A08;
+		padding: 0 58rpx;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+
+		.fundomianItem {
+			text-align: center;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+
+			.itemBg1 {
+				width: 60rpx;
+				height: 60rpx;
+				background: url("@/static/img/integralBg.png") 100% no-repeat;
+				background-size: 100% 100%;
+				margin-bottom: 12rpx;
+			}
+
+			.itemBg2 {
+				width: 60rpx;
+				height: 60rpx;
+				background: url("@/static/img/farmBg.png") 100% no-repeat;
+				background-size: 100% 100%;
+				margin-bottom: 12rpx;
+			}
+
+			.itemBg3 {
+				width: 60rpx;
+				height: 60rpx;
+				background: url("@/static/img/shoppingBg.png") 100% no-repeat;
+				background-size: 100% 100%;
+				margin-bottom: 12rpx;
+			}
+
+			.itemBg4 {
+				width: 60rpx;
+				height: 60rpx;
+				background: url("@/static/img/exchangBg.png") 100% no-repeat;
+				background-size: 100% 100%;
+				margin-bottom: 12rpx;
+			}
+		}
+	}
 </style>
